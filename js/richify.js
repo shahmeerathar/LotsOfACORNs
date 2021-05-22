1ed514878ed12e53974c195908168839df00db28
glowies <comuokta@gmail.com>
2021-05-22 16:36:43 -0400

    feat: Replace balance number!

--- js/richify.js
+++ js/richify.js
@@ -4,6 +4,15 @@ var intervalID = setInterval(function() {
     element = document.querySelector('[aria-label^="Your Credit Balance is "]');
     if (element != null) {
         clearInterval(intervalID);
-        console.log(element);
+        updateContent();
     }
 }, 1000);
+
+function updateContent() {
+    var max = 10000000;
+    var min = 600000;
+    var number = (max - min) * Math.random() + min;
+    var formatted = numeral(number).format("0,0.00");
+    console.log(formatted);
+    element.textContent =`CR $${formatted}`;
+}
